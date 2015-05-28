4471abb928254804749a2400959a78070b667fe3
Vladimir Bolshakov <vovanbo@gmail.com>
2015-05-28 16:34:14 +0300

    UMD compatibility

--- wNumb.js
+++ wNumb.js
@@ -1,4 +1,17 @@
-(function(){
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define([], factory);
+	} else if (typeof exports === 'object') {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like environments that support module.exports,
+		// like Node.
+		module.exports = factory();
+	} else {
+		// Browser globals (root is window)
+		root.wNumb = factory();
+	}
+}(this, function () {
 
 	'use strict';
 
@@ -330,6 +343,5 @@ var
 	}
 
 	/** @export */
-	window.wNumb = wNumb;
-
-}());
+	return wNumb;
+}));
